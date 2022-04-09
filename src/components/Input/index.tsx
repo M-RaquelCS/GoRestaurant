23cd5d89a8aaca6bad47a413587ecb1a361fe7e5
raquel <M-Raquel@outlook.com.br>
2022-04-09 11:41:49 -0300

    challenge finalized

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,24 +8,32 @@ import {
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
+import { IconType } from 'react-icons';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+interface InputProps {
+  name: string,
+  Icon?: IconType,
+  placeholder: string 
+}
+
+export function Input({ name, Icon, ...rest }: InputProps){
+
   const inputRef = useRef(null);
 
-  const [isFocused, setIsFocused] = useState(false);
-  const [isFilled, setIsFilled] = useState(false);
+  const [ isFocused, setIsFocused ] = useState(false)
+  const [ isFilled, setIsFilled ] = useState(false)
 
   const { fieldName, defaultValue, registerField } = useField(name);
 
   const handleInputFocus = useCallback(() => {
     setIsFocused(true);
-  }, []);
+  }, [])
 
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
-  }, []);
+    setIsFilled(!!inputRef.current); //?.value
+  }, [])
 
   useEffect(() => {
     registerField({
@@ -47,7 +55,5 @@ const Input = ({ name, icon: Icon, ...rest }) => {
         {...rest}
       />
     </Container>
-  );
-};
-
-export default Input;
+  ); 
+}
